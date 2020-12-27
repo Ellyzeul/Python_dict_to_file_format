387d9c4338a5256bc8ba4ac75c6f3bf3bcdd0b9a
Ellyzeul <gabrielaug.240@gmail.com>
2020-12-26 22:47:37 -0300

    Updating XLSX converter and it's test

--- src/dict_to_xlsx.py
+++ src/dict_to_xlsx.py
@@ -1,8 +1,30 @@
 from openpyxl import Workbook
 
 
-def dict_to_xlsx(src, keys, values):
+def append_to_xlsx_recursive(ws, dic, index):
+    if(isinstance(dic, list)):
+        # ...
+    dict_items = list(dic)
+    keys = dic.keys()
+    values = dic.values()
+    next_index = index
+
+    ws.append(keys)
+    index += 1
+
+    for value in values:
+        if(isinstance(value, dict)):
+            new_index = append_to_xlsx_recursive(ws, value, index)
+            if(new_index > next_index):
+                next_index = max([new_index, next_index])
+            continue
+        # ...
+
+def dict_to_xlsx(src):
     wb = Workbook()
+    ws = wb.active
+
+    append_to_xlsx_recursive(ws, src, 1)
 
     # code goes here...
 
--- test/test_dict_to_xlsx.py
+++ test/test_dict_to_xlsx.py
@@ -4,6 +4,17 @@ from dict_to_xlsx import dict_to_xlsx
 
 
 print('\n\nWorkbook allocated on:')
-print(dict_to_xlsx({}, [], []))
+print(dict_to_xlsx([{
+    'field_1': 'value_1_1',
+    'field_2': 'value_1_2',
+    'field_3': 'value_1_3',
+    'field_4': 'value_1_4'
+},
+{
+    'field_1': 'value_2_1',
+    'field_2': 'value_2_2',
+    'field_3': 'value_2_3',
+    'field_4': 'value_2_4'
+}]))
 
 print('\n')

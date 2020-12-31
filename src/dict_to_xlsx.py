788a394aac2634c65b48ba80bae3162390305288
Ellyzeul <gabrielaug.240@gmail.com>
2020-12-30 22:46:30 -0300

    Updating dict_to_xlsx.py

--- src/dict_to_xlsx.py
+++ src/dict_to_xlsx.py
@@ -1,30 +1,35 @@
-from openpyxl import Workbook
+from xlsxwriter import Workbook
+from dict_have_same_fields import dict_have_same_fields
 
 
-def append_to_xlsx_recursive(ws, dic, index):
+def append_to_xlsx_recursive(ws, dic, row=0, col=0, new_header=False):
     if(isinstance(dic, list)):
-        # ...
-    dict_items = list(dic)
-    keys = dic.keys()
-    values = dic.values()
-    next_index = index
+        if(dict_have_same_fields(dic[row], dic[row+1])):
+    
+    keys = list(dic.keys())
+    values = list(dic.values())
 
-    ws.append(keys)
-    index += 1
+    if(row == 0 or new_header):
+        for field in keys:
+            ws.write(col, row, field)
+            col += 1
+        row += 1
+        col = 0
+    next_row = row + 1
 
     for value in values:
         if(isinstance(value, dict)):
-            new_index = append_to_xlsx_recursive(ws, value, index)
-            if(new_index > next_index):
-                next_index = max([new_index, next_index])
+            new_row = append_to_xlsx_recursive(ws=ws, dic=value, row=row, col=col, new_header=True)
+            if(new_row > next_row):
+                next_row = max([new_row, next_row])
             continue
         # ...
 
-def dict_to_xlsx(src):
-    wb = Workbook()
-    ws = wb.active
+def dict_to_xlsx(src, workbook_name='converted_data.xlsx'):
+    wb = Workbook(workbook_name)
+    ws = wb.add_worksheet()
 
-    append_to_xlsx_recursive(ws, src, 1)
+    append_to_xlsx_recursive(ws, src)
 
     # code goes here...
 
